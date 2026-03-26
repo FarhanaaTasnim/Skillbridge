dd63a9234f89f0d4ea4af41e41f215ce4247c1be
FarhanaaTasnim <farhana.tasnim.993@gmail.com>
2026-03-27 02:45:16 +0600

    update CORS configuration to allow specific origins

--- skillbridge-backend/server.js
+++ skillbridge-backend/server.js
@@ -10,7 +10,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    process.env.FRONTEND_URL
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 connectDB();
