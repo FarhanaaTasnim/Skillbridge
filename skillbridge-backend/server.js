8b8ea63b080ea7d4fa0de8e88f6f5992bfd9fd5f
FarhanaaTasnim <farhana.tasnim.993@gmail.com>
2026-03-27 00:15:49 +0600

    fix server port binding for render

--- skillbridge-backend/server.js
+++ skillbridge-backend/server.js
@@ -1,19 +1,31 @@
 import express from "express";
 import cors from "cors";
-
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import jobRoutes from "./routes/jobRoutes.js";
 import resumeRoutes from "./routes/resumeRoutes.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+connectDB();
+
 app.use("/api/auth", authRoutes);
 app.use("/api/jobs", jobRoutes);
 app.use("/api/resume", resumeRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.get("/", (req, res) => {
+  res.send("SkillBridge API Running");
+});
+
+// THIS IS THE CRITICAL PART
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
